7e9574d998026ed0e0c1bdaf077353fdb8e3eb49
kruczjak <kruczjak@gmail.com>
2015-05-11 23:26:39 +0200

    1,2,3

--- app.js
+++ app.js
@@ -14,9 +14,23 @@
 var Fork = function() {
     this.state = 0;
     return this;
-}
+};
 
 Fork.prototype.acquire = function(cb) {
+    var state = this.state;
+
+    var acquireStart = function (sec, cb) {
+        setTimeout(function () {
+            if (state == 0) {
+                state = 1;
+                cb();
+            } else {
+                acquireStart(sec*2, cb);
+            }
+        }, sec);
+    };
+
+    acquireStart(1,cb);
     // zaimplementuj funkcję acquire, tak by korzystala z algorytmu BEB
     // (http://pl.wikipedia.org/wiki/Binary_Exponential_Backoff), tzn:
     // 1. przed pierwszą próbą podniesienia widelca Filozof odczekuje 1ms
@@ -31,7 +45,7 @@ Fork.prototype.release = function() {
 var Philosopher = function(id, forks) {
     this.id = id;
     this.forks = forks;
-    this.f1 = id % forks.length;
+    this.f1 = id % forks.lengtforks[f2]h;
     this.f2 = (id+1) % forks.length;
     return this;
 }
@@ -40,19 +54,52 @@ Philosopher.prototype.startNaive = function(count) {
     var forks = this.forks,
         f1 = this.f1,
         f2 = this.f2,
-        id = this.id,
+        id = this.id;
+
+
+    var cycle = function (c) {
+        forks[f1].acquire(function () {
+            forks[f2].acquire(function () {
+                //eating, omnomnom
+                forks[f1].release();
+                forks[f2].release();
+                if (c > 0)
+                    cycle(--c);
+            })
+        });
+    };
+
+    cycle(count);
 
     // zaimplementuj rozwiązanie naiwne
     // każdy filozof powinien 'count' razy wykonywać cykl
     // podnoszenia widelców -- jedzenia -- zwalniania widelców
-        }
+        };
 
     Philosopher.prototype.startAsym = function(count) {
         var forks = this.forks,
             f1 = this.f1,
             f2 = this.f2,
-            id = this.id,
+            id = this.id;
 
+        var cycle = function(c) {
+            if (id % 2 == 0) cycleInside(forks[f2], forks[f1]);
+            else cycleInside(forks[f1], forks[f2]);
+        };
+
+        var cycleInside = function(l,p,c) {
+            l.acquire(function () {
+                p.acquire(function () {
+                    //eating, omnomnom
+                    l.release();
+                    p.release();
+                    if (c > 0)
+                        cycleInside(l,p,--c);
+                })
+            });
+        };
+
+        cycle(count);
         // zaimplementuj rozwiązanie asymetryczne
         // każdy filozof powinien 'count' razy wykonywać cykl
         // podnoszenia widelców -- jedzenia -- zwalniania widelców
@@ -62,7 +109,7 @@ Philosopher.prototype.startNaive = function(count) {
             var forks = this.forks,
                 f1 = this.f1,
                 f2 = this.f2,
-                id = this.id,
+                id = this.id;
 
             // zaimplementuj rozwiązanie z kelnerem
             // każdy filozof powinien 'count' razy wykonywać cykl
@@ -72,7 +119,7 @@ Philosopher.prototype.startNaive = function(count) {
 
             var N = 5;
             var forks = [];
-            var philosophers = []
+            var philosophers = [];
             for (var i = 0; i < N; i++) {
                 forks.push(new Fork());
             }
@@ -82,5 +129,5 @@ Philosopher.prototype.startNaive = function(count) {
             }
 
             for (var i = 0; i < N; i++) {
-                philosophers[i].startNaive(10);
+                philosophers[i].startNaive(1000);
             }
