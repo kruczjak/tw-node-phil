e50a22f5eaad4ed36f6f02f96d1a6f63a9523dc7
kruczjak <kruczjak@gmail.com>
2015-05-14 00:18:30 +0200

    Better one

--- app.js
+++ app.js
@@ -57,6 +57,10 @@ Philosopher.prototype.startNaive = function(count) {
         id = this.id;
 
 
+    if (startTimesNaive[id] === undefined) {
+        startTimesNaive[id] = new Date().getTime();
+    }
+
     var cycle = function (c) {
         forks[f1].acquire(function () {
             forks[f2].acquire(function () {
@@ -65,6 +69,8 @@ Philosopher.prototype.startNaive = function(count) {
                 forks[f2].release();
                 if (c > 0)
                     cycle(--c);
+                else
+                    console.log(id, new Date().getTime() - startTimesNaive[id]);
             })
         });
     };
@@ -82,9 +88,13 @@ Philosopher.prototype.startNaive = function(count) {
             f2 = this.f2,
             id = this.id;
 
+        if (startTimesAsym[id] === undefined) {
+            startTimesAsym[id] = new Date().getTime();
+        }
+
         var cycle = function(c) {
-            if (id % 2 == 0) cycleInside(forks[f2], forks[f1]);
-            else cycleInside(forks[f1], forks[f2]);
+            if (id % 2 == 0) cycleInside(forks[f2], forks[f1], c);
+            else cycleInside(forks[f1], forks[f2], c);
         };
 
         var cycleInside = function(l,p,c) {
@@ -95,6 +105,8 @@ Philosopher.prototype.startNaive = function(count) {
                     p.release();
                     if (c > 0)
                         cycleInside(l,p,--c);
+                    else
+                        console.log(id, new Date().getTime() - startTimesAsym[id]);
                 })
             });
         };
@@ -110,6 +122,17 @@ Philosopher.prototype.startNaive = function(count) {
                 f1 = this.f1,
                 f2 = this.f2,
                 id = this.id;
+
+            if (startTimesConductor[id] === undefined) {
+                startTimesConductor[id] = new Date().getTime();
+            }
+
+            if (count != 0) {
+                conductor.ask(this, count);
+            } else {
+                console.log(id, new Date().getTime() - startTimesConductor[id]);
+            }
+
 
             // zaimplementuj rozwiązanie z kelnerem
             // każdy filozof powinien 'count' razy wykonywać cykl
@@ -131,3 +154,67 @@ Philosopher.prototype.startNaive = function(count) {
             for (var i = 0; i < N; i++) {
                 philosophers[i].startNaive(1000);
             }
+
+Philosopher.prototype.giveForks = function (count) {
+    var philosopher = this;
+    var r = Math.floor(Math.random() * this.eatTime);
+    startTimesConductor[philosopher.id] += r;
+    setTimeout(function () {
+        conductor.release(philosopher, count);
+    }, r);
+};
+
+var Conductor = function () {
+    this.queue = [];
+    return this;
+};
+
+Conductor.prototype.ask = function (philosopher, count) {
+    var id = philosopher.id,
+        f1 = philosopher.f1,
+        f2 = philosopher.f2,
+        forks = philosopher.forks;
+
+//    if (this.queue.length === 0) {
+    if (forks[f1].state === 0 && forks[f2].state === 0) {
+        forks[f1].state = 1;
+        forks[f2].state = 1;
+        philosopher.giveForks(count);
+    } else {
+        this.queue.push([id, count]);
+    }
+//    } else {
+//        this.queue.push([id, count]);
+//    }
+};
+
+Conductor.prototype.release = function (philosopher, count) {
+    var f1 = philosopher.f1,
+        f2 = philosopher.f2,
+        forks = philosopher.forks,
+        conductor = this;
+
+    forks[f1].state = 0;
+    forks[f2].state = 0;
+    philosopher.startConductor(count - 1);
+
+    var disposeQueue = function () {
+        if (conductor.queue.length !== 0) {
+            var id = conductor.queue[0][0],
+                count = conductor.queue[0][1],
+                f1 = philosophers[id].f1,
+                f2 = philosophers[id].f2,
+                forks = philosophers[id].forks;
+
+            if (forks[f1].state === 0 && forks[f2].state === 0) {
+                conductor.queue.shift();
+                forks[f1].state = 1;
+                forks[f2].state = 1;
+                philosophers[id].giveForks(count);
+                disposeQueue();
+            }
+        }
+    };
+
+    disposeQueue();
+};
